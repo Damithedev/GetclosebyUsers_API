ed8d6bbdbc30ae756d6c1344501a843fda35416c
damilola <adamidogo@gmail.com>
2024-05-12 21:30:09 +0100

    update

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 
 import firebase_admin
 
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore,messaging
 # Fetch the service account key JSON file contents
 cred = credentials.Certificate('secretkey.json')
 # Initialize the app with a service account, granting admin privileges
@@ -9,3 +9,14 @@ app = firebase_admin.initialize_app(cred)
 
 store = firestore.client()
 doc_ref = store.collection(u'users')
+
+def send_push_notification(token, title, body, imgurl):
+    # Create a message
+    message = messaging.Message(
+        notification=messaging.Notification(title=title, body=body, image=imgurl),
+        token=token,
+    )
+
+    # Send the message
+    response = messaging.send(message)
+    print("Successfully sent message:", response)
--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from fastapi import FastAPI
-from database import doc_ref
+from database import doc_ref, send_push_notification
 import google.cloud
 from geopy.distance import geodesic
 
-app = FastAPI()
-user1 = {'name': 'User1', 'location': (52.52, 13.405)}
-user2 = {'name': 'User2', 'location': (51.51, 7.467)}
-user3 = {'name': 'User3', 'location': (48.8566, 2.3522)}
 
-# Example list of users
-users = [user1, user2, user3]
+app = FastAPI()
+
 @app.get("/")
 async def root():
     try:
@@ -20,17 +16,25 @@ async def root():
         print(u'Missing data')
 
 @app.get("/nearby")
-def get_nearby_coordinates(longitude: float, latitude: float):
+def get_nearby_coordinates(longitude: float, latitude: float, uid:str):
     nearby_users = []
+    tokens = []
     #rmm
     docs = doc_ref.get()
     for doc in docs:
         user = doc.to_dict()
         print(user)
+        user_id = user['uid']
         user_lat = user['lat']
         user_lng = user['lng']
+        if user_id == uid:
+            continue
         distance = geodesic((latitude, longitude), (user_lat, user_lng)).kilometers
+
         print(user_lat, user_lng)
         if distance <= 100:
+            user['distance']= distance
             nearby_users.append(user)
+    for nearby_user in nearby_users:
+        send_push_notification(nearby_user['FCM'], f'{nearby_user["firstname"]} needs Help', f'{nearby_user["distance"]} Miles away', imgurl=user['profile picture'])
     return nearby_users
