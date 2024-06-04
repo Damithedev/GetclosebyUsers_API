716f84ce9b28a2986f0b6c457f7601d84ee75008
damilola <adamidogo@gmail.com>
2024-06-04 13:59:51 +0100

    update

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI
 from database import doc_ref, send_push_notification
 import google.cloud
 from geopy.distance import geodesic
+import socketio
 
 
 app = FastAPI()
-
+sio=socketio.AsyncServer(cors_allowed_origins='*',async_mode='asgi')
+#wrap with ASGI application
+socket_app = socketio.ASGIApp(sio)
 @app.get("/")
 async def root():
     try:
@@ -45,3 +48,18 @@ def get_nearby_coordinates(longitude: float, latitude: float, uid:str):
 
         send_push_notification(nearby_user['FCM'], f'{username} needs Help', f'{nearby_user["distance"]} Miles away', imgurl=userpic, uid= uid, distance= f'{nearby_user["distance"]}')
     return nearby_users
+
+
+@sio.on("connect")
+async def connect(sid, env):
+      print("New Client Connected to This id :"+" "+str(sid))
+      await sio.emit("send_msg", "Hello from Server", to='2339djjd')
+@sio.on("disconnect")
+async def disconnect(sid):
+    print("Client Disconnected: "+" "+str(sid))
+
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(socket_app, host='0.0.0.0', port=8000)
